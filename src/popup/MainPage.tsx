b8731ea3d8fe6b511b2558c0e8a5dfbbe77d44ac
Natsugure <natsugure5301102@gmail.com>
2026-06-06 16:39:13 +0900

    進捗率オーバーレイ表示を実装

--- src/popup/MainPage.tsx
+++ src/popup/MainPage.tsx
@@ -1,15 +1,34 @@
-import { Button } from "@mantine/core";
+import { Box, Button, Container, Flex, LoadingOverlay, Progress, Space, Text } from "@mantine/core";
 import { useUpdateStats } from "./hooks/useUpdateStats";
 
 export function MainPage() {
-  const { updateStats } = useUpdateStats()
+  const { updateStats, isProcessing, progress } = useUpdateStats()
 
   const onClickFetchStats = () => {
-    updateStats()
+    updateStats().then(
+      // TODO: 完了通知+エラー通知を入れる
+    )
   }
 
   return (
     <>
+    <LoadingOverlay 
+      visible={isProcessing}
+      loaderProps={{ 
+        children: (
+          <Container>
+            <Flex mb="xs" justify="space-between">
+              <Text>更新中…</Text>
+              <Space />
+              <Text>{progress.toFixed(1)}%</Text>
+            </Flex>
+            <Box w={500}>
+              <Progress value={progress} />
+            </Box>
+          </Container>
+        )
+      }}
+    />
       <Button onClick={onClickFetchStats}>統計を取得</Button>
     </>
   )
--- src/popup/hooks/useUpdateStats.ts
+++ src/popup/hooks/useUpdateStats.ts
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { useApiClient } from "./useApiClient"
 import { useUser } from "./useUsers"
 import { fetchArticles, fetchCurrentUser, fetchStats } from "@/lib/noteApi"
@@ -17,20 +18,28 @@ interface UpdateStatsRequest {
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 export function useUpdateStats() {
+  const [isProcessing, setIsProcessing] = useState(false)
+  const [progress, setProgress] = useState(0)
+
   const fetchedAt = new Date()
   const api = useApiClient()
   const { fetchUser } = useUser()
 
   const updateStats = async () => {
+    setIsProcessing(true)
+
     const statsResponse: NoteStatsResponse = []
     const articlesResponse: NoteArticlesResponse = []
 
     try {
       const currentUser = await fetchCurrentUser()
+
       const articleCount = currentUser.note_count
       const statsPerPage = 10
       const statsTotalPage = Math.ceil(articleCount / statsPerPage)
-      
+      /** Stats取得時の1ページあたりの進捗割合。*/
+      const statsProgressPerPage = statsTotalPage > 0 ? 50 / statsTotalPage : 50
+
       for (let i = 1; i <= statsTotalPage; i++) {
         console.log(`fetchStats: Page ${i}`)
         const res = await fetchStats(i)
@@ -49,15 +58,28 @@ export function useUpdateStats() {
         }
 
         statsResponse.push(...res.note_stats)
+        // 割り切れない値で50を超えないようにする
+        setProgress(Math.min(i * statsProgressPerPage, 50))
         if (i < statsTotalPage) await sleep(500)
       }
       
       // note_listには下書きも含まれていて、currentUserのnote_countと値が異なる。
       // そのため、note_listの応答に含まれるtotalCountの値とisLastPageのフラグを使って制御する。
+      const articlePerPage = 16
       let page = 1
+      let articleProgressPerPage = 0
       while (true) {
         const res = await fetchArticles(page)
         articlesResponse.push(...res.notes)
+
+        if (page === 1) {
+          const totalPages = Math.ceil(res.totalCount / articlePerPage)
+          articleProgressPerPage = totalPages > 0 ? 50 / totalPages : 50
+        }
+
+        // stats終了分の50を常に足す。そして、割り切れない値で100を超えないようにする。
+        setProgress(Math.min(page * articleProgressPerPage + 50, 100))
+
         if (res.isLastPage) break
         page++
         await sleep(500)
@@ -87,6 +109,7 @@ export function useUpdateStats() {
         }]
       })
 
+      // TODO: API送信部分も進捗率計算に含める
       for (const item of requestBody) {
         console.log(`updateStats: ${item.params.noteArticleId}, publishAt: ${item.body.article.publishedAt}`)
         await api.POST("/api/me/articles/{noteArticleId}/stats", {
@@ -100,7 +123,9 @@ export function useUpdateStats() {
       }
     } catch (e) {
       console.error(e)
+    } finally {
+      setIsProcessing(false)
     }
   }
-  return { updateStats }
+  return { updateStats, isProcessing, progress }
 }
