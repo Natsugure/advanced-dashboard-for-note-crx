de5a1e18151bfe29098ec83637237fb77dc2f4a4
Natsugure <natsugure5301102@gmail.com>
2026-06-12 08:46:06 +0900

    ESLintを導入

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -12,11 +13,15 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      react.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error",
     },
   },
 ])
