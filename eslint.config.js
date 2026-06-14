e7c3c4c71fad17820704dc9631fce6b47b6255fc
Natsugure <natsugure5301102@gmail.com>
2026-06-14 09:45:37 +0900

    ESLintに型情報が反映されていなかった問題を修正

--- eslint.config.js
+++ eslint.config.js
@@ -9,19 +9,26 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['src/**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      react.configs.recommended,
+      react.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
+      ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      }
     },
     rules: {
       "@typescript-eslint/no-floating-promises": "error",
+      "no-unused-expressions": ["error", { "allowTernary": true }],
     },
   },
 ])
